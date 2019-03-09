0f185b9be96a75c442d83710301a0d8b5174ff71
Yungchih Chen <yungchih_chen@yahoo.com>
2019-03-09 15:39:30 -0500

    Add Start War characters list example

---
+++ src/containers/CharacterPage.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { connect }from 'react-redux'
+import { IAppState } from '../store'
+import { ICharacter } from '../reducers/CharacterReducers'
+
+// Create the containers interface
+export interface IProps {
+  characters: ICharacter[]
+}
+
+class CharacterPage extends React.Component<IProps, any> {
+  public render() {
+    console.log('this.props inside of CharacterList container', this.props)
+    const { characters } = this.props
+    return (
+      <div className="name-container">
+        <h3>The Force Awakens</h3>
+        {characters && characters.map(character => {
+          return(
+            <>
+              <span key={character.name} className="name">
+                {character.name}
+              </span><br/>
+            </>
+          )
+        })}
+      </div>
+    );
+  }
+}
+
+// Grab the characters from the store and make them available on props
+const mapStateToProps = (store: IAppState) => {
+  return {
+    characters: store.characterState.characters
+  }
+}
+
+export default connect(mapStateToProps)(CharacterPage)
